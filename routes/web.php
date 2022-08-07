67a9516c9b37c0289622c4ccaf4d8879d7804c31
Syahal Tastari <sahaltastari15@gmail.com>
2022-08-08 02:37:26 +0700

    feat: slicing detail movie

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,9 @@ Route::prefix('prototype')->name('prototype.')->group(function () {
     route::get('subscriptionPlan', function () {
         return Inertia::render('Prototype/SubscriptionPlan');
     })->name('subscriptionPlan');
+    route::get('movie/{sllug}', function () {
+        return Inertia::render('Prototype/Movie/Show');
+    })->name('movie.show');
 });
 
 require __DIR__.'/auth.php';
